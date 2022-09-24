2e12761835bb78680bfd07da5c54703aea190a3e
Aldo <acasareto@gmail.com>
2022-09-24 11:12:57 +0200

    some css practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import './App.css';
+import ChallengeOne from './components/challenges/challenge1/ChallengeOne';
 import { Application } from './components/Application/Application';
 import Counter from './components/Counter/Counter';
 import MuiMode from './components/mui/Mui-mode';
 import { Skills } from './components/Skills/Skills';
 import Users from './components/users/Users';
 import AppProvider from './providers/appProvider';
+import Challenge2 from './components/challenges/challenge1/Challenge2';
+import Challenge3 from './components/challenges/Challenge3';
 
 // import Greet from './components/Greet/Greet';
 
 function App() {
   return (
-    <AppProvider>
-      <div className='App'>
-        <Application />
-        <Skills skills={['HTML', 'CSS', 'JS', 'NodeJS']} />
-        <Counter />
-        <MuiMode />
-      </div>
-      <Users />
-    </AppProvider>
+    // <AppProvider>
+    //   <div className='App'>
+    //     <Application />
+    //     <Skills skills={['HTML', 'CSS', 'JS', 'NodeJS']} />
+    //     <Counter />
+    //     <MuiMode />
+    //   </div>
+    //   <Users />
+    // </AppProvider>
+    // <ChallengeOne />
+    // <Challenge2 />
+    <Challenge3 />
   );
 }
 
---
+++ src/components/challenges/Challenge3.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './style3.css';
+
+type Props = {};
+
+const Challenge3 = (props: Props) => {
+  return (
+    <div className='intro'>
+      <div className='container'>
+        <div className='intro-text'>
+          <h1>Resonsive layouts don't have to be a struggle</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam.
+          </p>
+          <button>I WANT TO LEARN</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Challenge3;
